7fc831fbc1de9d4b93602e291d9b8b5002a354a4
stjmm <shetrynajerkme@gmail.com>
2026-03-28 12:11:40 +0100

    exercises from chapter 2

--- include/clrs/insertion_sort.hpp
+++ include/clrs/insertion_sort.hpp
@@ -31,4 +31,20 @@ namespace clrs {
             A[j + 1] = key;
         }
     }
+
+    template<typename T>
+    void insertion_sort_recursive(std::vector<T> &A, int n) {
+        if (n == 0)
+            return;
+        insertion_sort_recursive(A, n - 1);
+
+        int j = n - 1;
+        T key = A[n];
+        while (j >= 0 && A[j] > key) {
+            A[j + 1] = A[j];
+            j--;
+        }
+
+        A[j + 1] = key;
+    }
 }
--- src/chapter02/sorts.cpp
+++ src/chapter02/sorts.cpp
@@ -20,5 +20,10 @@ int main()
     clrs::merge_sort(arr, 0, arr.size() - 1);
     clrs::print_vector(arr, "merge sort");
 
+
+    arr = {5,2,4,6,1,3};
+    clrs::insertion_sort_recursive(arr, arr.size() - 1);
+    clrs::print_vector(arr, "recursive insertion sort");
+
     return 0;
 }
