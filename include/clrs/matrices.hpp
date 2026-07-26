e583027955d262c27195eb3465021791cf81e978
stjmm <shetrynajerkme@gmail.com>
2026-07-26 11:08:56 +0200

    recursive matrix multiplciation cpp

--- include/clrs/matrices.hpp
+++ include/clrs/matrices.hpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 
 namespace clrs {
@@ -17,5 +16,33 @@ namespace clrs {
         }
 
         return C;
+    }
+
+    void matrix_multiply_recursive(const Matrix &A, const Matrix &B,
+            Matrix &C, int aRow, int aCol, int bRow, int bCol,
+            int cRow, int cCol, int n)
+    {
+        if (n == 1) {
+            C[cRow][cCol] += A[aRow][aCol] * B[bRow][bCol];
+            return;
+        }
+
+        const int half = n / 2;
+
+        // C11 = A11*B11 + A12*B21
+        matrix_multiply_recursive(A, B, C, aRow, aCol, bRow, bCol, cRow, cCol, half);
+        matrix_multiply_recursive(A, B, C, aRow, aCol + half, bRow + half, bCol, cRow, cCol, half);
+
+        // C12 = A11*B12 + A12*B22
+        matrix_multiply_recursive(A, B, C, aRow, aCol, bRow, bCol + half, cRow, cCol + half, half);
+        matrix_multiply_recursive(A, B, C, aRow, aCol + half, bRow + half, bCol + half, cRow, cCol + half, half);
+
+        // C21 = A21*B11 + A22*B21
+        matrix_multiply_recursive(A, B, C, aRow + half, aCol, bRow, bCol, cRow + half, cCol, half);
+        matrix_multiply_recursive(A, B, C, aRow + half, aCol + half, bRow + half, bCol, cRow + half, cCol, half);
+
+        // C22 = A21*B12 + A22*B22
+        matrix_multiply_recursive(A, B, C, aRow + half, aCol, bRow, bCol + half, cRow + half, cCol + half, half);
+        matrix_multiply_recursive(A, B, C, aRow + half, aCol + half, bRow + half, bCol + half, cRow + half, cCol + half, half);
     }
 }
--- src/chapter04/matrices.cpp
+++ src/chapter04/matrices.cpp
@@ -3,11 +3,15 @@
 
 int main()
 {
-    std::vector<std::vector<int>> A {{2, 4}, {5, 6}};
-    std::vector<std::vector<int>> B {{2, 4}, {5, 6}};
+    clrs::Matrix A {{2, 4}, {5, 6}};
+    clrs::Matrix B {{2, 4}, {5, 6}};
 
     clrs::Matrix C = clrs::matrix_multiply(A, B, 2);
-    clrs::print_matrix(C, 2);
+    // clrs::print_matrix(C, 2);
+
+    clrs::Matrix D (2, std::vector<int>(2, 0));
+    clrs::matrix_multiply_recursive(A, B, D, 0, 0, 0, 0, 0, 0, 2);
+    clrs::print_matrix(D, 2);
 
     return 0;
 }
