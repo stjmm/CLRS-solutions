ca0abeb0e2492089bdcf6ea56a61222f909f8617
stjmm <shetrynajerkme@gmail.com>
2026-07-30 12:17:25 +0200

    heap change

--- include/clrs/heap.hpp
+++ include/clrs/heap.hpp
@@ -1,49 +1,49 @@
 #include <algorithm>
-#include <cstddef>
 #include <vector>
 
 namespace clrs {
 
 template <typename T>
-void max_heapify(std::vector<T>& values, std::size_t heap_size,
-                 std::size_t root) {
-  const std::size_t left = 2 * root + 1;
-  const std::size_t right = 2 * root + 2;
+void max_heapify(std::vector<T>& A, int heap_size, int root) {
+  const int left = 2 * root + 1;
+  const int right = 2 * root + 2;
 
-  std::size_t largest = root;
+  int largest = root;
 
-  if (left < heap_size && values[left] > values[largest]) {
+  if (left <= heap_size && A[left] > A[largest]) {
     largest = left;
   }
 
-  if (right < heap_size && values[right] > values[largest]) {
+  if (right <= heap_size && A[right] > A[largest]) {
     largest = right;
   }
 
   if (largest != root) {
-    std::swap(values[root], values[largest]);
-    max_heapify(values, heap_size, largest);
+    std::swap(A[root], A[largest]);
+    max_heapify(A, heap_size, largest);
   }
 }
 
 template <typename T>
-void build_max_heap(std::vector<T>& values) {
-  if (values.size() < 2) {
+void build_max_heap(std::vector<T>& A) {
+  const int size = static_cast<int>(A.size());
+
+  for (int i = size / 2 - 1; i >= 0; --i) {
+    max_heapify(A, size - 1, i);
+  }
+}
+
+template <typename T>
+void heap_sort(std::vector<T>& A) {
+  if (A.size() < 2) {
     return;
   }
 
-  for (std::size_t i = values.size() / 2; i > 0; --i) {
-    max_heapify(values, values.size(), i - 1);
-  }
-}
+  build_max_heap(A);
 
-template <typename T>
-void heap_sort(std::vector<T>& values) {
-  build_max_heap(values);
-
-  for (std::size_t heap_size = values.size(); heap_size > 1; --heap_size) {
-    std::swap(values[0], values[heap_size - 1]);
-    max_heapify(values, heap_size - 1, 0);
+  for (int i = static_cast<int>(A.size()) - 1; i >= 1; --i) {
+    std::swap(A[0], A[i]);
+    max_heapify(A, i - 1, 0);
   }
 }
 
